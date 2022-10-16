33d4069d18ebd0f640884f9fe7461f46ee706e87
NgocNguyenC3 <nguyen.dn.hubtech@gmail.com>
2022-10-16 23:23:21 +0700

    fix

--- lib/ui/login/login_page.dart
+++ lib/ui/login/login_page.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:lettutor_edu_clone/res/constants/local_string.dart';
 import 'package:lettutor_edu_clone/res/dimens.dart';
 import 'package:lettutor_edu_clone/res/gen/assets.gen.dart';
 import 'package:lettutor_edu_clone/ui/base/base_scaffold.dart';
@@ -25,7 +26,7 @@ class LoginPage extends GetWidget<LoginController> {
               height: 35.h,
             ),
             Assets.images.imageLogin.image(),
-            Text(LocalString.)
+            Text(LocalString.loginTitle)
           ],
         ),
       ),
